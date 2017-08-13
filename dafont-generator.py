a03320b16dc356112977ff260c8cfe7a194c10d9
idbentley <ian.bentley@gmail.com>
2017-08-13 09:56:50 -0400

    add new fonts and script for generating font packages from dafont.com

--- dafont-generator.py
+++ dafont-generator.py
@@ -10,7 +10,7 @@ pkgrel=1
 pkgdesc=""
 arch=(any)
 license=('')
-url=''
+url='{url}'
 depends=('fontconfig' 'xorg-font-utils')
 source=('{dl_url}')
 install=$pkgname.install
@@ -46,15 +46,18 @@ if __name__ == "__main__":
 
     pkgname = args.url[len("http://www.dafont.com/"):-len(".font")]
 
-    dl_url = "http://dl.dafont.com/dl/?f=octin_spraypaint_free"#"http://dl.dafont.com/dl/?f={}".format(pkgname)
+    dl_url = "http://dl.dafont.com/dl/?f={}".format(pkgname.replace("-", "_"))
 
     headers = {
+        'Host': "dl.dafont.com",
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
         'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         'Accept-Language': "en-US,en;q=0.5",
         'Accept-Encoding': "gzip, deflate",
-        'Referer': args.url
-        }
+        'Referer': args.url,
+        'Connection': "keep-alive",
+        'Upgrade-Insecure-Requests': "1"
+    }
 
     req = requests.get(dl_url, headers=headers)
 
@@ -70,5 +73,5 @@ if __name__ == "__main__":
         fd.write(install_template)
 
     with open("{}/PKGBUILD".format(pkgname), "w") as fd:
-        fd.write(pkgbuild_template.format(pkgname=pkgname, dl_url=dl_url, md5=md5sum))
+        fd.write(pkgbuild_template.format(pkgname=pkgname, url=args.url, dl_url=dl_url, md5=md5sum))
 
